451e878ed017efcb3c6cdaf836f1a087117b9067
GalacticPast <192919385+GalacticPast@users.noreply.github.com>
2026-02-12 12:35:19 -0600

    latex and treesitter context

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,22 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-        ensure_installed = {
-            "c",
-            "cpp",
-            "lua",
+    -- Core Treesitter
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        opts = {
+            ensure_installed = { "c", "cpp", "lua" },
+            highlight = { enable = true },
+            indent = { enable = true },
         },
-        indent = { enable = true },
-        highlight = { enable = true },
-        folds = { enable = true },
+    },
+
+    {
+        "nvim-treesitter/nvim-treesitter-context",
+        opts = {
+            enable = true,
+            line_numbers = true,
+            mode = 'cursor',
+            max_lines = 3, -- Just an example to keep it tidy
+        }
     }
 }
